4c7ca33d3bf8ca4f5619b91f746c288cf23858e2
Gao QingLiang <1033192926@qq.com>
2025-03-10 17:27:48 +0800

    feat:loading size

--- src/components/common/loadingData/index.tsx
+++ src/components/common/loadingData/index.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import LoadingSVG from '../loadingSVG';
 
 const LoadingData: React.FC = () => {
-    return <LoadingSVG />;
+    return (
+        <div className="w-full h-full flex flex-col items-center justify-center ">
+            <div className="w-20 h-20">
+                <LoadingSVG />
+            </div>
+        </div>
+    );
 };
 
 export default LoadingData;
